60d81f70cfa8ed6665aa906b22807bfc70f06c45
CUdex <cuyu9779@naver.com>
2021-01-09 09:08:50 +0900

    fixed by warmachine_test

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: cryu <cryu@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 08:54:45 by cryu              #+#    #+#             */
-/*   Updated: 2020/12/25 08:59:09 by cryu             ###   ########.fr       */
+/*   Updated: 2021/01/09 08:58:47 by cryu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t i;
 
 	i = 0;
+	if (n == 0)
+		return (0);
 	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
